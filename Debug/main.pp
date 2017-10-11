1fb7a9855d888a12b3d0195aad57f432c384a7fc
tengyong1012 <tengyong1012@163.com>
2017-10-11 16:52:59 +0800

    change all

---
+++ Debug/main.pp
@@ -0,0 +1,49 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: ../main.h
+main.obj: D:/F28335_WorkSpace/28335Module/Dev/Scia/scia.h
+main.obj: D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_Device.h
+main.obj: D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_Adc.h
+main.obj: D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_DevEmu.h
+main.obj: D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_CpuTimers.h
+main.obj: D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_ECan.h
+main.obj: D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_ECap.h
+main.obj: D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_DMA.h
+main.obj: D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_EPwm.h
+main.obj: D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_EQep.h
+main.obj: D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_Gpio.h
+main.obj: D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_I2c.h
+main.obj: D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_Mcbsp.h
+main.obj: D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_PieCtrl.h
+main.obj: D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_PieVect.h
+main.obj: D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_Spi.h
+main.obj: D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_Sci.h
+main.obj: D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_SysCtrl.h
+main.obj: D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_XIntrupt.h
+main.obj: D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_Xintf.h
+main.obj: D:/F28335_WorkSpace/28335Module/Dev/Gpio/gpio.h
+
+../main.c: 
+../main.h: 
+D:/F28335_WorkSpace/28335Module/Dev/Scia/scia.h: 
+D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_Device.h: 
+D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_Adc.h: 
+D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_DevEmu.h: 
+D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_CpuTimers.h: 
+D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_ECan.h: 
+D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_ECap.h: 
+D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_DMA.h: 
+D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_EPwm.h: 
+D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_EQep.h: 
+D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_Gpio.h: 
+D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_I2c.h: 
+D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_Mcbsp.h: 
+D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_PieCtrl.h: 
+D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_PieVect.h: 
+D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_Spi.h: 
+D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_Sci.h: 
+D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_SysCtrl.h: 
+D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_XIntrupt.h: 
+D:/F28335_WorkSpace/28335Module/DSP2833x_headers/include/DSP2833x_Xintf.h: 
+D:/F28335_WorkSpace/28335Module/Dev/Gpio/gpio.h: 
